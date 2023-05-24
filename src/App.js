89b8703447deef9702e9e7bce1ee697d853b3f29
ikukojohanna <johanna.odersky@gmail.com>
2023-05-24 11:11:14 +0200

    remove dots

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <p>Under Construction... </p>
+                <p>Under Construction </p>
             </header>
         </div>
     );
